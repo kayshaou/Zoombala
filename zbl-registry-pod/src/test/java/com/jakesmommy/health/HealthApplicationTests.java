5a128c68ce44e0451c537c35d3ad9d408a7337bc
kayshaou <kaysha.ou@gmail.com>
2022-08-21 18:37:48 +0800

    folder structure

---
+++ zbl-registry-pod/src/test/java/com/jakesmommy/health/HealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakesmommy.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
